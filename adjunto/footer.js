1a4ac70626219b514702694c5d980e4c7842bd38
MilagrosGonza <130479298+MilagrosGonza@users.noreply.github.com>
2023-04-11 12:46:57 -0300

    Update footer.js

--- adjunto/footer.js
+++ adjunto/footer.js
@@ -10,14 +10,14 @@ footerHTML.innerHTML += `
         <nav>
             <div>
                 <h3>Desarrollado por:</h3>
-                <p>Bramajo Lautaro</p>
-                <p>Galarza Gonzalo</p>
+                <p>----</p>
+                <p>----</p>
             </div>
             <div>
                 <h3>Contacto</h3>
-                <a href="https://chat.whatsapp.com/JhmA05le4K2A4BpvMH7Beu" class="what"><i class="bi bi-whatsapp"></i> Whatsapp</a>
-                <a href="https://www.youtube.com/@argentinaprograma4.0" class="youtube"><i class="bi bi-youtube"></i> Youtube</a>
-                <a href="https://discord.gg/QBCFkzTcvJ" class="discord"><i class="bi bi-discord"></i> Discord</a>
+                <a href="https://chat.whatsapp.com/" class="what"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+                <a href="https://www.youtube.com/" class="youtube"><i class="bi bi-youtube"></i> Youtube</a>
+                <a href="https://discord.gg/" class="discord"><i class="bi bi-discord"></i> Discord</a>
             </div>
         </nav>
         <br>
